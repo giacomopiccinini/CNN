dba32ddf192dc0471ec4f0cd96bbc1992b188a01
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-10-03 17:00:05 +0200

    [ADD] Add logging and splitting

---
+++ main.py
@@ -0,0 +1,9 @@
+from Code.Modules.A_split import split
+
+import logging
+
+if __name__ == '__main__':
+
+    logging.info("Loading datasets")
+    (train_set, labels_train), (validation_set, labels_validation), (test_set, labels_test) = split()
+
