69c0a8949f6ade558643e611fa96ffad6256431d
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-10-04 15:55:35 +0200

    [UPDATE]

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Code.Parser.parser   import parse
 from Code.Modules.A_split import split
 
 import logging
@@ -5,9 +6,10 @@ import logging
 if __name__ == '__main__':
 
     logging.info("Parsing requests")
+    args = parse()
 
     logging.info("Loading datasets")
-    (train_set, labels_train), (validation_set, labels_validation), (test_set, labels_test) = split()
+    (train_set, labels_train), (validation_set, labels_validation), (test_set, labels_test) = split(args["Split"])
 
     logging.info("Preparing network")
 
