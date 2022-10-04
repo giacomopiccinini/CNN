fd07f9a6444b2cb3e2d22672385674177776ebc0
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-10-04 12:18:10 +0200

    [UPDATE]

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import logging
 
 if __name__ == '__main__':
 
+    logging.info("Parsing requests")
+
     logging.info("Loading datasets")
     (train_set, labels_train), (validation_set, labels_validation), (test_set, labels_test) = split()
+
+    logging.info("Preparing network")
+
 
