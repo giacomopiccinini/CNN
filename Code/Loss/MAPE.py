e3ff5cf84942287a9fed7b7927c85d7b2fd86286
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-10-04 12:59:59 +0200

    [ADD] Add MAPE loss

---
+++ Code/Loss/MAPE.py
@@ -0,0 +1,20 @@
+from tf.keras.losses import MeanAbsolutePercentageError
+import logging
+
+def loss(**kwargs):
+
+    """ Create Mean Absolute Percentage Error (MAPE) loss """
+
+    try:
+        # Create loss if correct keywords are passed
+        Loss = MeanAbsolutePercentageError(**kwargs)
+
+        # Log the loading
+        logging.info("Mean Absolute Percentage Error loss has been loaded")
+
+        # Return the optimizer
+        return Loss
+
+    except Exception as e:
+        # Raise an exception if wrong keywords are passed
+        raise e
