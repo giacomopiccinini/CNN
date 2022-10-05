ddd3a5d3219fda35c22a39f3a7ecb88fd31a81e3
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-10-05 15:32:49 +0200

    [FIX]

--- Code/Modules/C_train.py
+++ Code/Modules/C_train.py
@@ -1,7 +1,14 @@
+from Code.CometML.track_experiment import track
+
 def train(CNN, train_set, validation_set, args):
 
     """ Train CNN """
 
-    # Train CNN
-    CNN.fit(train_set,validation_data=validation_set, epochs=args.epochs)
+    experiment = track(args)
+
+    # Declare training environment
+    with experiment.context_manager("train"):
+
+        # Train CNN
+        CNN.fit(train_set,validation_data=validation_set, epochs=args["Train"].epochs)
 
