e3d24924dd1561032027f7e755d8da7818c4a87a
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-10-05 13:51:17 +0200

    [FIX] Fix y to be np.array

--- Code/Modules/C_train.py
+++ Code/Modules/C_train.py
@@ -1,7 +1,8 @@
-def train(CNN, train_set, labels_train, validation_set, labels_validation, args):
+def train(CNN, train_set, validation_set, args):
 
     """ Train CNN """
 
     # Train CNN
-    CNN.fit(train_set, labels_train, validation_data=(validation_set, labels_validation), epochs=args.epochs, batch_size=args.batch)
+    #CNN.fit(train_set,validation_data=validation_set, epochs=args.epochs, batch_size=args.batch)
+    CNN.fit(train_set,validation_data=validation_set, epochs=args.epochs)
 
