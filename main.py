f3881d076b62c16981289607b94d439d0dba6152
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-10-05 11:15:48 +0200

    [UPDATE]

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import logging
+
 from Code.Parser.parser     import parse
+
 from Code.Modules.A_split   import split
 from Code.Modules.B_prepare import prepare
-
-import logging
+from Code.Modules.C_train   import train
 
 if __name__ == '__main__':
 
@@ -10,9 +12,13 @@ if __name__ == '__main__':
     args = parse()
 
     logging.info("Loading datasets")
-    #(train_set, labels_train), (validation_set, labels_validation), (test_set, labels_test) = split(args["Split"])
+    (train_set, labels_train), (validation_set, labels_validation), (test_set, labels_test) = split(args["Split"])
+    shape = train_set.shape
 
     logging.info("Preparing network")
-    prepare(args["Train"])
+    CNN = prepare(args=args["Prepare"], shape=shape)
+
+    logging.info("Training network")
+    train(CNN, train_set, labels_train, validation_set, labels_validation, args["Train"])
 
 
